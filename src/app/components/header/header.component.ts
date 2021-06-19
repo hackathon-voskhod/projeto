c487e1f37d534d2f242dc085e6e0be4e54d819ed
Paula Eli Sinigaglia <paulaelisini@hotmail.com>
2021-06-19 12:51:00 -0300

    adição do componente de header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  @Input() public titulo: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
