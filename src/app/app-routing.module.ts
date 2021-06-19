071a095fbdd11a95ad2ad9370a482d8e0b2dfe61
Hugo Sobral <hugosobral10@gmail.com>
2021-06-19 15:03:06 -0300

    Inicio da criaçao dos estilos globais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { InstituicoesPageComponent } from './components/instituicoes-page/instituicoes-page.component';
+import { LeisPageComponent } from './components/leis-page/leis-page.component';
 import { MenuNavegacaoComponent } from './components/menu-navegacao/menu-navegacao.component';
 
 const routes: Routes = [
@@ -16,6 +17,10 @@ const routes: Routes = [
       {
         path: 'instituicoes',
         component: InstituicoesPageComponent,
+      },
+      {
+        path: 'leis',
+        component: LeisPageComponent,
       },
     ],
   },
